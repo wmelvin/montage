d278046588746d19901a25bc93ad80e50366fbe5
Bill Melvin <bill@billmelvin.com>
2021-08-12 13:19:12 -0400

    Can create and save an image file.

--- montage.py
+++ montage.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+from PIL import Image
 
-print("All I can do now is print this message. More code, please!")
+file_name = 'output.jpg'
 
+canvas_width = 640
+canvas_height = 480
+canvas_size = (canvas_width, canvas_height)
+
+bg_color = (0, 64, 0)  # (red, green, blue)
+
+image = Image.new('RGB', canvas_size, bg_color)
+
+print(f"\nSaving {file_name}.")
+
+image.save(file_name)

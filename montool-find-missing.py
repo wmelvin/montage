d94ee6d30580f7ffa912d02dfb98ba0fdb21bff0
Bill Melvin <bill@billmelvin.com>
2021-09-20 10:59:22 -0400

    Build list of ImageListItem instances to use for searching and reporting results.

--- montool-find-missing.py
+++ montool-find-missing.py
@@ -5,9 +5,56 @@ import sys
 from pathlib import Path
 
 
-app_version = "210919.1"
+app_version = "210920.1"
 
 app_title = f"montool-find-missing.py (version {app_version})"
+
+
+class ImageListItem:
+    def __init__(self, list_name, original_path):
+        self.list_name = list_name
+        self.original_path = original_path
+        p = Path(original_path).expanduser().resolve()
+        self.path_expanded = str(p)
+        self.original_exists = p.exists()
+        self.tried_to_find = False
+        self.new_path = None
+
+    def __str__(self):
+        return f"ImageListItem('{self.list_name}', '{self.original_path}')"
+
+    def as_str(self):
+        s = f"  {self.list_name=}\n"
+        s += f"  {self.original_path=}\n"
+        s += f"  {self.path_expanded=}\n"
+        s += f"  {self.original_exists=}\n"
+        s += f"  {self.tried_to_find=}\n"
+        s += f"  {self.new_path=}\n"
+        return s
+
+    def do_find(self):
+        return not (self.original_exists or self.tried_to_find)
+
+
+def get_args():
+    ap = argparse.ArgumentParser(
+        description="Search for missing image files listed in a "
+        + "settings/options file for montage.py."
+    )
+
+    ap.add_argument("opt_file", help="Name of settings/options file.")
+
+    ap.add_argument(
+        "-s",
+        "--search-dir",
+        dest="search_dir",
+        type=str,
+        action="store",
+        help="Optional. Directory to start search. Default is to search the "
+        + "parent path of the current image file path.",
+    )
+
+    return ap.parse_args()
 
 
 def get_option_entries(opt_section, opt_content):
@@ -50,24 +97,7 @@ def try_to_find(file_path: Path) -> str:
 def main():
     print(f"\n{app_title}\n")
 
-    ap = argparse.ArgumentParser(
-        description="Search for missing image files listed in a "
-        + "settings/options file for montage.py."
-    )
-
-    ap.add_argument("opt_file", help="Name of settings/options file.")
-
-    ap.add_argument(
-        "-s",
-        "--search-dir",
-        dest="search_dir",
-        type=str,
-        action="store",
-        help="Optional. Directory to start search. Default is to search the "
-        + "parent path of the current image file path.",
-    )
-
-    args = ap.parse_args()
+    args = get_args()
 
     opt_path = Path(args.opt_file).resolve()
     if not opt_path.exists():
@@ -82,39 +112,52 @@ def main():
             sys.exit(1)
 
     print(f"Reading {args.opt_file}.")
-    #  Not really! :}~
 
     with open(opt_path, "r") as f:
         file_text = f.readlines()
 
-    opt_img_b = get_option_entries("[background-settings]", file_text)
-    opt_img_f = get_option_entries("[images]", file_text)
-    opt_img_1 = get_option_entries("[images-1]", file_text)
+    list_items = []
 
-    print(f"{opt_img_b=}")
-    print(f"{opt_img_1=}")
+    list_items += [
+        ImageListItem("background-images", x)
+        for x in get_option_entries("[background-images]", file_text)
+    ]
 
-    out_list = []
+    list_items += [
+        ImageListItem("images", x)
+        for x in get_option_entries("[images]", file_text)
+    ]
 
-    out_list.append("[images]")
+    list_items += [
+        ImageListItem("images-1", x)
+        for x in get_option_entries("[images-1]", file_text)
+    ]
 
-    for fn in opt_img_f:
-        out_list.append("")
-        p = Path(fn).resolve()
-        if p.exists():
-            out_list.append(str(p))
-        else:
-            out_list.append(f"# {fn}")
-            print(f"Not found: {p}")
-            ph = try_to_find(p)
-            if 0 < len(ph):
-                out_list.append(ph)
-            else:
-                out_list.append("NOT FOUND")
+    for i in list_items:
+        print(i)
+        print(f"{i.as_str()}\n")
 
-    with open("output-find-missing.txt", "w") as f:
-        for item in out_list:
-            f.write(f"{item}\n")
+    # out_list = []
+
+    # out_list.append("[images]")
+
+    # for fn in opt_img_f:
+    #     out_list.append("")
+    #     p = Path(fn).resolve()
+    #     if p.exists():
+    #         out_list.append(str(p))
+    #     else:
+    #         out_list.append(f"# {fn}")
+    #         print(f"Not found: {p}")
+    #         ph = try_to_find(p)
+    #         if 0 < len(ph):
+    #             out_list.append(ph)
+    #         else:
+    #             out_list.append("NOT FOUND")
+
+    # with open("output-find-missing.txt", "w") as f:
+    #     for item in out_list:
+    #         f.write(f"{item}\n")
 
 
 if __name__ == "__main__":
